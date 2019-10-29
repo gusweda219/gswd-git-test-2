15b383a030a7da4f4a927a00cd3c8f8c352bd4fe
gusweda219 <wedabaskara219@gmail.com>
2019-10-29 09:27:29 +0800

    menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore repellat culpa quo, cumque quasi dolorum totam excepturi dolore vel sequi quas non voluptas! Quo dolorum nemo cupiditate natus ea animi!</p>
 </body>
 </html>
